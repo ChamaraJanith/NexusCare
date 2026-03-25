554db30cf34e2cac2b95dbd677cc3af2b0bb521f
ShashiDissanayake <sewminishashikala781@gmail.com>
2026-03-25 23:47:00 +0530

    feat(controller): add appointment controller with service integration

--- backend/appointment-service/src/services/appointmentService.js
+++ backend/appointment-service/src/services/appointmentService.js
@@ -1,17 +1,22 @@
 const Appointment = require("../models/Appointment");
 
+// Create Appointment
 exports.createAppointment = async (data) => {
-  return await Appointment.create(data);
+  const appointment = new Appointment(data);
+  return await appointment.save();
 };
 
+// Get appointments by patient
 exports.getAppointmentsByPatient = async (patientId) => {
   return await Appointment.find({ patientId });
 };
 
+// Update appointment
 exports.updateAppointment = async (id, data) => {
   return await Appointment.findByIdAndUpdate(id, data, { new: true });
 };
 
+// Cancel appointment
 exports.cancelAppointment = async (id) => {
   return await Appointment.findByIdAndUpdate(
     id,
